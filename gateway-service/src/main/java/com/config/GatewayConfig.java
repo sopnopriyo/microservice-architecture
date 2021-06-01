8cb825eb6ac95d73ab8afa330bb5e36b36bfb1c4
Shahin Alam <sopnopriyo@gmail.com>
2021-06-01 12:39:10 +0800

    Added Liquibase to authentication service

--- gateway-service/src/main/java/com/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/config/GatewayConfig.java
@@ -17,10 +17,9 @@ public class GatewayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-//                .route("user-service", r -> r.path("/users/**")
-//                        .filters(f -> f.filter(filter))
-//                        .uri("lb://user-service"))
-
+                .route("user-service", r -> r.path("/users/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://user-service"))
                 .route("auth-service", r -> r.path("/auth/**", "/users/**", "/oauth2/**")
                         .filters(f -> {
                             f.addRequestHeader("x-forwarded-for", "localhost");
